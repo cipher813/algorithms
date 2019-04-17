534197b534fdc200384d810164dd6e7ce637d1cf
Brian McMahon <30627595+cipher813@users.noreply.github.com>
2019-04-16 21:14:02 -0700

    add dfs

--- search.py
+++ search.py
@@ -1,13 +1,18 @@
 from collections import defaultdict
 
-class BreadthFirstSearch:
-    """Breadth first search"""
+class Graph:
+    """Generic graph class"""
     def __init__(self):
         self.graph = defaultdict(list)
         
     def addEdge(self, u, v):
         self.graph[u].append(v)
-        
+
+class BreadthFirstSearch(Graph):
+    """Breadth first search"""
+    def __init__(self):
+        super().__init__()
+   
     def BFS(self, s):
         visited = [False] * (len(self.graph))
         queue = []
@@ -15,8 +20,25 @@ class BreadthFirstSearch:
         visited[s] = True
         while queue:
             s = queue.pop(0)
-            print(s, end = " ")
+            print(f"{s} explored")
             for i in self.graph[s]:
                 if visited[i] == False:
                     queue.append(i)
                     visited[i] = True
+
+class DepthFirstSearch(Graph):
+    """Depth first search"""
+    def __init__(self):
+        super().__init__()
+        
+    def DFSUtil(self, v,visited):
+        visited[v] = True
+        print(f"{v} explored")
+        
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.DFSUtil(i, visited)
+                
+    def DFS(self, v):
+        visited = [False] * (len(self.graph))
+        self.DFSUtil(v,visited)
